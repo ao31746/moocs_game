9e241a1567f0a3b6499485386a1ecfe9b9317f7d
peterpan1011288 <peterpan1011288@gmail.com>
2017-09-26 13:18:36 +0800

    add ajax to price war game and company merger game.

--- moocs_games/games/views.py
+++ moocs_games/games/views.py
@@ -8,18 +8,14 @@ def ROFR(request):
     return render(request, 'ROFR.html', {
         'data': "Hello Django ",
     })
-
-
 def JUDO(request):
     return render(request, 'JUDO.html', {
         'data': "Hello Django ",
     })
-
 def Company_Merger(request):
     return render(request, 'Company_Merger.html', {
         'data': "Hello Django ",
     })
-
 def Price_War(request):
     return render(request, 'Price_War.html', {
         'data': "Hello Django ",
@@ -30,15 +26,12 @@ def Price_War(request):
 class ROFR_ViewSet(viewsets.ModelViewSet):
     queryset = ROFR_model.objects.all()
     serializer_class = ROFR_Serializer
-
 class JUDO_ViewSet(viewsets.ModelViewSet):
     queryset = JUDO_model.objects.all()
     serializer_class = JUDO_Serializer
-
 class Company_Merger_ViewSet(viewsets.ModelViewSet):
     queryset = Company_Merger_model.objects.all()
     serializer_class = Company_Merger_Serializer
-
 class Price_War_ViewSet(viewsets.ModelViewSet):
     queryset = Price_War_model.objects.all()
     serializer_class = Price_War_Serializer
